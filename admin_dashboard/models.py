d2a2834ddff3866911901555c401bdc8ea133462
Aya-Ramdan <ayaramadan799@gmail.com>
2025-04-06 14:55:27 +0200

    Implemented AdminSelectedProjects Model

--- admin_dashboard/models.py
+++ admin_dashboard/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from projects.models import Project
 
 # Create your models here.
+
+class AdminSelectedprojects(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Admin Selecte: {self.project.title}"
