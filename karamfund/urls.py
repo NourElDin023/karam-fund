5baa45213ca9b32531029ac7add66ae1be3e3684
NourElDin Nabil <nournabil1012@gmail.com>
2025-04-09 12:40:29 +0200

    Add home view and template, update URL routing

--- karamfund/urls.py
+++ karamfund/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from karamfund.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
     path('users/', include('users.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
