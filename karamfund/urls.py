776efcd2d67750d47665a39dc045952b7a36fe7b
Ahmed-Mansy <Ahmedalimansy1@gmail.com>
2025-04-11 03:08:19 +0200

    make add project html urls

--- karamfund/urls.py
+++ karamfund/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('users/', include('users.urls')),
+    path('projects/', include('projects.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import *
+
 
 # Create your views here.
+
+
+
+def addProject(req):
+    
+    
+    
+    
+    
+    return render(req,'add_project.html')
